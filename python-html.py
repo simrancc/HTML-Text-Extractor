75172512fe69db1c1dad82b3b5b3208a4954c1bc
simrancc <simrancc@umich.edu>
2020-05-23 17:53:41 -0500

    added new paragraphs and punctuation

--- python-html.py
+++ python-html.py
@@ -9,34 +9,34 @@ with open("sephoraPP.html") as fp:
 #def dataLoaded(err, data) :
   #$ = cheerio.load('' + data + '');
   #console.log($.html($('ul')[0]));
-
+removed_strings = []
 for x in soup.find_all("p"):
     temp = str(x.contents[0].string)
     strr = ":"
     if (temp.rfind(strr) == len(temp) - 1 or temp.rfind(strr) == len(temp) - 2) :
         for sibling in x.next_siblings:
             if sibling.name == "li" or sibling.name == "ul" or sibling.name == "ol" :
-                count = 0
                 #for child in sibling.descendants :
                     #print(child.parent)
                 for child in sibling.stripped_strings:
-                    print(repr(child))
-                    print()
+                    #print(repr(child))
+                    #print()
                     #ellipsis_doc = nlp(temp)
                     #ellipsis_sentences = list(ellipsis_doc.sents)
                     #for sentence in ellipsis_sentences:
                         #print(sentence)
                         #print()
-                    new_tag = soup.new_tag("p")
-                    x.append(new_tag)
-                    new_tag.string = child
-                    #print(soup)
-                    #if type(child) != type(x.contents[0].string) :
-                        #for y in child.descendants :
-                            #if type(child) != type(x.contents[0].string)
-                            #print(y)
-                            #print("yayyayayya")
-                            #combine = ""
-                            #for string in y.strings :
-                                #combine = combine + " " + string
-                            #print(combine)
+                    hello = sibling.find_all(string=child)
+                    if (child not in hello) :
+                        if (child.rfind(".") != len(child) - 1 and child.rfind(".") != len(child) - 2) :
+                            child += "."
+                        new_tag = soup.new_tag("p")
+                        x.parent.append(new_tag)
+                        new_tag.string = child
+                    else :
+                        removed_strings.append(child)
+                sibling.extract()
+                x.extract()
+                
+with open("output1.html", "w") as file:
+    file.write(str(soup))
