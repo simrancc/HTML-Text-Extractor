95305fe26609f3894b5f04fbcea04c0c98f2e675
simrancc <simrancc@umich.edu>
2020-05-24 14:30:55 -0500

    got last sentence and appended it with the list element

--- python-html.py
+++ python-html.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
-#import spacy
+import spacy
 
-#nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_sm')
 with open("sephoraPP.html") as fp:
     soup = BeautifulSoup(fp, features="html.parser")
     #print(soup)
@@ -16,27 +16,23 @@ for x in soup.find_all("p"):
     if (temp.rfind(strr) == len(temp) - 1 or temp.rfind(strr) == len(temp) - 2) :
         for sibling in x.next_siblings:
             if sibling.name == "li" or sibling.name == "ul" or sibling.name == "ol" :
-                #for child in sibling.descendants :
-                    #print(child.parent)
+                ellipsis_doc = nlp(temp)
+                ellipsis_sentences = list(ellipsis_doc.sents)
+                lastSentence = ellipsis_sentences[len(ellipsis_sentences) - 1]
+                #print(lastSentence)
                 for child in sibling.stripped_strings:
-                    #print(repr(child))
-                    #print()
-                    #ellipsis_doc = nlp(temp)
-                    #ellipsis_sentences = list(ellipsis_doc.sents)
-                    #for sentence in ellipsis_sentences:
-                        #print(sentence)
-                        #print()
                     hello = sibling.find_all(string=child)
                     if (child not in hello) :
                         if (child.rfind(".") != len(child) - 1 and child.rfind(".") != len(child) - 2) :
                             child += "."
                         new_tag = soup.new_tag("p")
                         x.parent.append(new_tag)
-                        new_tag.string = child
+                        new_tag.string = str(lastSentence) + " " + child
+                        print(new_tag.string)
                     else :
                         removed_strings.append(child)
                 sibling.extract()
                 x.extract()
-                
+
 with open("output1.html", "w") as file:
     file.write(str(soup))
