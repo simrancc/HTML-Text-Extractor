96a6b0f50d26b3af06dd51c29d6379345c13b27c
simrancc <simrancc@umich.edu>
2020-06-01 12:56:27 -0500

    use html2text

--- extractData-html.py
+++ extractData-html.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import spacy
 import re
 import sys
+import html2text
+
 
 class PrivacyPolicy(object):
 
@@ -16,25 +18,17 @@ class PrivacyPolicy(object):
 
     #only print plain text for whichever html passed in
     def print_plain_text(self, html) :
-        #open html and create BS object to parse
-        with open(html) as fp:
-            soup = BeautifulSoup(fp, features="html.parser")
-        #loop through all strings within entire html file then print
-        for strings in soup.stripped_strings:
-            print(strings)
+        html = open(html).read()
+        print(html2text.html2text(html))
 
     #store plain text into an output file for whichever html passed in
     def output_plain_text(self, html) :
-        #open html and create BS object to parse
-        with open(html) as fp:
-            soup = BeautifulSoup(fp, features="html.parser")
-        allStrings = []
-        #loop through all strings within entire html file and store into array
-        for strings in soup.stripped_strings:
-            allStrings.append(strings)
-        #write allStrings to output file
-        with open("plaintext.txt", "w") as file:
-            file.write(str(allStrings))
+        # Open your file
+        with open(html, 'r') as f_html:
+            html = f_html.read()
+        # Open a file and write to it
+        with open('plaintext.txt', 'w') as f:
+            f.write(html2text.html2text(html))
 
 
     def simplify_html(self) :
@@ -45,7 +39,7 @@ class PrivacyPolicy(object):
             text = str(x.contents[0].string) #get text of <p>
             colon = ":"
             #find <p> that end in a colon
-            if (text.rfind(colon) == len(text) - 1 or text.rfind(strr) == len(text) - 2) :
+            if (text.rfind(colon) == len(text) - 1 or text.rfind(colon) == len(text) - 2) :
                 for sibling in x.next_siblings:
                     #find sibling that is li/ul/ol
                     if sibling.name == "li" or sibling.name == "ul" or sibling.name == "ol" :
@@ -60,30 +54,30 @@ class PrivacyPolicy(object):
                             #print("############################################")
                             allStrings = sibling.find_all(string=re.compile(child))
                             #print(allStrings)
-                        for string in allStrings :
-                            #print(string)
-                            #if parent of string is <li> include
-                            if (string.parent.name == "li") :
-                                #check if ending of list text is correct punctuation otherwise add '.'
-                                if ( (string.rfind(".") != len(string) - 1 and string.rfind(".") != len(string) - 2) and
-                                (string.rfind("?") != len(string) - 1 and string.rfind("?") != len(text) - 2) and
-                                (string.rfind("!") != len(string) - 1 and string.rfind("!") != len(text) - 2) ):
-                                    string += "."
-                                #create new paragraph element and insert before orginial <p>
-                                new_tag = self.inputSoup.new_tag("p")
-                                x.insert_before(new_tag)
-                                new_tag.string = str(lastSentence) + " " + string
-                                #print(new_tag.string)
-                            #if parent is not <li> it is not a list element (subtitle, comment, etc.)
-                            else :
-                                self.removed_strings.append(string)
-                        #delete entire bulleted list and original <p>
+                            for strings in allStrings :
+                                #print(strings)
+                                #if parent of string is <li> include
+                                if (strings.parent.name == "li") :
+                                    #check if ending of list text is correct punctuation otherwise add '.'
+                                    if ( (strings.rfind(".") != len(strings) - 1 and strings.rfind(".") != len(strings) - 2) and
+                                    (strings.rfind("?") != len(strings) - 1 and strings.rfind("?") != len(strings) - 2) and
+                                    (strings.rfind("!") != len(strings) - 1 and strings.rfind("!") != len(strings) - 2) ):
+                                        strings += "."
+                                        #create new paragraph element and insert before orginial <p>
+                                    new_tag = self.inputSoup.new_tag("p")
+                                    sibling.insert_before(new_tag)
+                                    new_tag.string = str(lastSentence) + " " + strings
+                                        #print(self.inputSoup)
+                                        #print(new_tag.string)
+                                        #if parent is not <li> it is not a list element (subtitle, comment, etc.)
+                                else :
+                                    self.removed_strings.append(strings)
+                        #delete entire bulleted list
                         sibling.extract()
-                        x.extract()
 
     #output html
     def outputFile(self) :
-        with open("output1.html", "w") as file:
+        with open("output2.html", "w") as file:
             file.write(str(self.inputSoup))
 
     #calc average list of length
@@ -132,4 +126,4 @@ html = sys.argv[1]
 remove_Arr = []
 p = PrivacyPolicy(html, remove_Arr, 0, 0)
 p.simplify_html()
-print(p.inputSoup.prettify())
+p.output_plain_text("output3.html")
